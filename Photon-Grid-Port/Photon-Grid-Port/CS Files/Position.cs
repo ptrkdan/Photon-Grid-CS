c4e6940ed5c9d6f14be24718835a86a05f571310
andrewjhill0 <andrewjhill0@gmail.com>
2015-03-11 23:23:26 +0900

    Ported Position.cs

--- Photon-Grid-Port/Photon-Grid-Port/CS Files/Position.cs
+++ Photon-Grid-Port/Photon-Grid-Port/CS Files/Position.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Photon_Grid_Port.CS_Files
 {
-    class Position
+    public class Position : ISerializable
     {
+
+        public int x;
+        public int y;
+
+        Position(int newX, int newY)
+        {
+            x = newX;
+            y = newY;
+        }
+
     }
 }
